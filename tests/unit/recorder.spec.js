11c23d6511945be93040c5c1ed3d7cbed9618e59
builder555 <85308587+builder555@users.noreply.github.com>
2021-07-08 21:47:18 -0400

    added proper MediaRecorder initialization

--- tests/unit/recorder.spec.js
+++ tests/unit/recorder.spec.js
@@ -1,16 +1,36 @@
 import SoundRecorder from '@/js/recorder';
 
-describe('SoundRecorder', () => {
-  it('should start recording audio', () => {
-    const startRecord = jest.fn();
-    Object.defineProperty(window, 'MediaRecorder', {
-      value: jest.fn().mockImplementation(() => ({
-        start: startRecord,
-      })),
-    });
+const stub = (object) => ({
+  constructor: (constructorName) => ({
+    with(value) {
+      Object.defineProperty(object, constructorName, {
+        value: jest.fn().mockImplementation(() => value),
+      });
+    },
+  }),
+  property: (propertyName) => ({
+    with(value) {
+      Object.defineProperty(object, propertyName, { value });
+    },
+  }),
+});
 
+describe('SoundRecorder', () => {
+  const mockStartRecord = jest.fn();
+  beforeAll(() => {
+    stub(window).constructor('MediaRecorder').with({ start: mockStartRecord });
+  });
+  it('should process getUserMedia promise to record audio', async () => {
     const recorder = new SoundRecorder();
+    const mockUserMedia = jest.fn();
+    stub(window.navigator).property('mediaDevices').with({ getUserMedia: mockUserMedia });
+    await recorder.init();
+    expect(mockUserMedia).toHaveBeenCalledWith({ audio: true });
+  });
+  it('should start recording audio by calling .start() method of MediaRecorder', async () => {
+    const recorder = new SoundRecorder();
+    await recorder.init();
     recorder.start();
-    expect(startRecord).toHaveBeenCalled();
+    expect(mockStartRecord).toHaveBeenCalled();
   });
 });
